59e59324e2ba3b826d6ea93617877a030a14230b
Massimo Ridella <mridella.ieu2022@student.ie.edu>
2025-06-03 21:57:52 +0200

    tests

--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -8,7 +8,9 @@ def test_dummy_wrong_path(testing_client):
     THEN check the response is valid
     """
     # Exercise III: Develop this test according to the definition and make it pass in the GitHub workflow
-
+    with app.test_client() as client:
+        response = client.get('/wrong_path')
+        assert response.status_code == 404
 def test_get_characters(testing_client):
     """
     GIVEN a Flask application
@@ -16,7 +18,8 @@ def test_get_characters(testing_client):
     THEN check the response is valid
     """
     # Exercise III: Develop this test according to the definition and make it pass in the GitHub workflow
-
+    response = testing_client.get('/characters')
+    assert response.status_code == 200
 def test_create_character(testing_client):
     """
     GIVEN a Flask application
@@ -24,5 +27,7 @@ def test_create_character(testing_client):
     THEN check the response is valid with status code 200
     """
     # Exercise III: Develop this test according to the definition and make it pass in the GitHub workflow
+    response = testing_client.post('/characters', json={'alias': 'John Doe', 'name': 'John Doe'})
+    assert response.status_code == 200
 
 
